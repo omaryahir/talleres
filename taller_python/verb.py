ffb37d4356df667adc0045c94939e4950906ba8a
Omar Mendoza <omaryahir@gmail.com>
2014-12-09 16:45:43 -0600

    ADD: practices

---
+++ taller_python/verb.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# get verb
+
+verb = raw_input("Escriba el verbo:")
+
+pronom = {
+	"yo": "o",
+	"tu": "as",
+	"el": "a",
+	"nosotros": "amos",
+	"vosotros": "áis",
+	"ellos": "an",
+}
+
+print "Yo %s%s" % (verb[0:-2],pronom["yo"])
+print "Tú %s%s" % (verb[0:-2],pronom["tu"])
+print "Él %s%s" % (verb[0:-2],pronom["el"])
+print "Nosotros %s%s" % (verb[0:-2],pronom["nosotros"])
+print "Vosotros %s%s" % (verb[0:-2],pronom["vosotros"])
+print "Ellos %s%s" % (verb[0:-2],pronom["ellos"])
+
---
+++ taller_python/verb_2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# get verb
+
+verb = raw_input("Escriba el verbo:")
+
+pronom = ["Yo", "Tú", "Él", "Nosotros", "Vosotros", "Ellos"]
+
+endverb = {
+	"Yo": "o",
+	"Tú": "as",
+	"Él": "a",
+	"Nosotros": "amos",
+	"Vosotros": "áis",
+	"Ellos": "an",
+}
+
+for i in pronom:
+	print "%s %s%s" % (i, verb[0:-2], endverb[i])
+
