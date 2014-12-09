701ba6081957ccc9a75f5b5fed900c11f18bce65
Omar Mendoza <omaryahir@gmail.com>
2014-12-08 18:25:45 -0600

    20141208--1825

--- taller_angularjs/servidor.js
+++ taller_angularjs/servidor.js
@@ -1,4 +1,7 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static("angularjs")
-).listen(3000); 
+var connect = require('connect'),
+		serveStatic = require('serve-static');
+
+var app = connect();
+
+app.use(serveStatic("pruebas_angular"));
+app.listen(3000);
