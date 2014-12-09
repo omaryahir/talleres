9b5439a474ebaa6ec1bdb002bcc574a0be4b145c
Omar Mendoza <omaryahir@gmail.com>
2014-12-09 16:31:06 -0600

    ADD: Python practices

---
+++ taller_python/calculate_days.py
@@ -0,0 +1,24 @@
+# -*- coding: utf8 -*-
+
+# request number of seconds.
+seconds = raw_input("Dígite los segundos:")
+
+try:
+	seconds = long(seconds)
+
+	if seconds <= 1000000:
+		minutes = seconds / 60
+		hours = minutes / 60
+		days = hours / 24
+	
+		print "Minutos: %d" % minutes
+		print "Horas: %d" % hours
+		print "Días: %d" % days
+	else:
+		print "El número es mayor a 1000000"
+
+
+except: 
+	print "Número no válido"
+	
+
---
+++ taller_python/media.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+
+# Media of 3 numbers
+
+maxahora = 0
+suma = 0
+for i in [1,2,3]:
+	number = raw_input("Escribe el número:")
+
+	try:
+		num = float(number)
+	except:
+		print "Número no válido"
+		
+	if maxahora < num:
+		maxahora = num
+
+	suma = suma + num
+
+prom = suma / 3
+
+print "Número máximo: %s | Promedio: %s | Suma: %s" % (maxahora,prom,suma)
