1d03a03d3e8d5888f05b75cabfa9e6c9325bd1ef
omaryahir <omaryahir@gmail.com>
2016-07-08 16:05:56 -0500

    ADD: part1

--- taller_angularjs/part1/controller.js
+++ taller_angularjs/part1/controller.js
@@ -3,9 +3,4 @@
 *
 * Description
 */
-dependencies_modules = ["ngResources"];
-var app = angular.module('MyFirstApp', depedencies_modules);
-
-app.controller("MyFirstController", function($scope) {
-    $scope.nombre="Omar Mendoza";
-});
+angular.module('MyFirstApp', []);
