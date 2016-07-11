bab7cb5eb5c70c265c81524cb4f854b972230558
omaryahir <omaryahir@gmail.com>
2016-07-10 21:53:49 -0500

    ADD: watchCollections

--- taller_angularjs/part6-todolist/controller.js
+++ taller_angularjs/part6-todolist/controller.js
@@ -11,21 +11,17 @@ angular.module("Todolst",["LocalStorageModule"])
             fecha: '01-01-2016 10:00 am'
         }
     */
+    $scope.$watchCollection('todo',function(newValue, oldValue){
+        localStorageService.set("angular-todolist",$scope.todo);
+    });
     $scope.addAct = function (){
         $scope.todo.push($scope.newAct);
         $scope.newAct = {};
-        localStorageService.set("angular-todolist",$scope.todo);
     }
     $scope.delAct = function (){
         $scope.todo = [];
-        localStorageService.set("angular-todolist",$scope.todo);
     }
 
-    $scope.$watch(function() {
-        return $scope.newAct;
-    }, function(newValue, oldValue) {
-        console.log(newValue);
-        console.log(oldValue);
-    });
+
 
 });
