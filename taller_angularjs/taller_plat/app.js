f28347f72f06534f333a26193160ae644c32b4b3
omaryahir <omaryahir@gmail.com>
2016-08-07 21:37:55 -0500

    ADD: using tabs

--- taller_angularjs/taller_plat/app.js
+++ taller_angularjs/taller_plat/app.js
@@ -11,11 +11,19 @@
       speed: '90',
       force: '80',
       skill: '99',
-      techniques: [ 'Chilena', 'Tiro de remate' ],
+      techniques: [ 'Chilena', 'Tiro de remate', 'Team' ],
       image: 'http://www.topkool.com/fr/wp-content/uploads/2012/12/olive-et-tom-014.jpg',
-      birthdate: '1970-09-10T17:10:10'
+      birthdate: '1970-09-10T17:10:10',
+      champeon:
+        [
+          { year:'1980', position:'1er'},
+          { year:'1982', position:'TIE'},
+          { year:'1984', position:'1er'},
+          { year:'1985', position:'1er'},
+          { year:'1990', position:'1er'},
+          { year:'1995', position:'1er'},
+        ]
     }
-
   });
 
 })();
