0f4885ca5000ba9e88c3d6849b71b3a7f55abbb6
omaryahir <omaryahir@gmail.com>
2016-08-07 21:02:53 -0500

    Adding filters

--- taller_angularjs/taller_plat/app.js
+++ taller_angularjs/taller_plat/app.js
@@ -1,13 +1,21 @@
 (function() {
-    var app=angular.module('supercampeones', []);
+  
+  var app=angular.module('supercampeones', []);
+  app.controller('PlayerController', function (){
+    this.player = {
+      id: 001,
+      name: 'Oliver',
+      number: '10',
+      position: 'Delantero',
+      kick: '99',
+      speed: '90',
+      force: '80',
+      skill: '99',
+      techniques: [ 'Chilena', 'Tiro de remate' ],
+      image: 'http://www.topkool.com/fr/wp-content/uploads/2012/12/olive-et-tom-014.jpg',
+      birthdate: '1970-09-10T17:10:10'
+    }
 
-    app.controller('PlayerController', function () {
-        this.pokemon = {
-            kick: '99',
-            speed: '90',
-            force: '80',
-            skill: '80'
-        }
-    });
+  });
 
 })();
