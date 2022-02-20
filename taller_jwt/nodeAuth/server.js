4076babeb02ed6f0d1a4547a6882606e3a0e5cd8
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2022-02-20 09:30:14 -0600

    feat: server is running on localhost 8080

--- taller_jwt/nodeAuth/server.js
+++ taller_jwt/nodeAuth/server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-var corsOptions = (
+var corsOptions = {
     origin: "https://localhost:8081"
-);
+};
 
 app.use(cors(corsOptions));
 // parse request to json
 app.use(express.json());
 // parse application/x-www-form-encoded
-app.use(express.urlencoded({ extended: true });
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
  res.json({ message: "welcome my friend!" });
@@ -18,6 +18,6 @@ app.get("/", (req, res) => {
 
 const PORT=process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log('Server running on PORT ${PORT}.'); 
+    console.log(`Server >> running on PORT ${PORT}.`); 
 });
 
