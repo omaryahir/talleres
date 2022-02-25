bd5b44669e1f68b68b2368240fa116938673c74c
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2022-02-24 21:39:04 -0600

    feat: adding user role validation

--- taller_jwt/nodeAuth/middleware/auth.Jwt.js
+++ taller_jwt/nodeAuth/middleware/auth.Jwt.js
@@ -2,4 +2,52 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
 const db = require("../models");
 
+const User = db.user;
+
+verifyToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+    if (!token) {
+        return res.status(403).send({
+            message: "No token provided!"
+        });
+    }
+    jwt.verify(token, coonfig.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: "Unauthorized!"
+            });
+        }
+        req.userId = decoded.id
+        next();
+    });
+}
+
+isRoleInUser = (role, userId) => {
+  User.findByPk(userId).then( user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
+
+
+isAdmin = (req, res, next) => {
+  if (isRoleInUser(req.userId, "admin") {
+    next();
+  } else {
+    res.status(403).send({
+      message: "Require Admin Role!"
+    });
+  }
+} 
+
+
+isModerator = (req, res, next) 
+
+
 
