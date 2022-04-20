b01f5e67565c4e4f0c61065ca78d7e512b67bcb1
omaryahir <1206997+omaryahir@users.noreply.github.com>
2022-04-19 21:20:32 -0500

    feat: how to find in a hash/associative array

--- taller_typescript/Hash.ts
+++ taller_typescript/Hash.ts
@@ -11,3 +11,12 @@ teamPoints['Python'] = 1;
 
 console.log(teamPoints);
 
+if ('Python' in teamPoints) {
+  console.log('Yep Python is in teamPoints');
+}
+
+if (!('Java' in teamPoints)) {
+  console.log('Nop Java is not in teamPoints');
+}
+
+
