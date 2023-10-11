69e79e3efbe9543df979228376a577d8b3ecc5c8
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2023-10-11 10:52:05 -0600

    feat: here we can see the advantage of using the interface

--- taller_next/next-with-ts/react-with-ts/myproject/src/components/Button.tsx
+++ taller_next/next-with-ts/react-with-ts/myproject/src/components/Button.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
 
 interface Props {
-    value: string
+    value: string;
+    onClick: () => void;
 }
 
-const Button: FC<Props> = ({ value }): JSX.Element => {
-    return <button className="calc-btn border">{ value }</button>
+const Button: FC<Props> = ({ value, onClick }): JSX.Element => {
+    return <button className="calc-btn border" onClick={onClick}>{ value }</button>
 }
