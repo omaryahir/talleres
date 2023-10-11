4a60d2338ea7990b9637f4f041d8602912844db0
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2023-10-11 11:24:57 -0600

    feat: implementing ButtonWrapper component

--- taller_next/next-with-ts/react-with-ts/myproject/src/components/Calculator.tsx
+++ taller_next/next-with-ts/react-with-ts/myproject/src/components/Calculator.tsx
@@ -1,38 +1,41 @@
 import { FC } from "react";
 import Button from "./Button";
+import ButtonWrapper from "./ButtonWrapper";
 
 const Calculator: FC = (): JSX.Element => {
-    return <div className="border padding-10">
+    return (
+      <div className="border padding-10">
         <div className="calculator-screen border">
             <span>0</span>
             <span className="result">0</span>
         </div>
-        <div className="btn-wrapper">
+        <ButtonWrapper>
             <Button value="1" />
             <Button value="2" />
             <Button value="3" />
-        </div>
-        <div className="btn-wrapper">
+        </ButtonWrapper>
+        <ButtonWrapper>
             <Button value="4" />
             <Button value="5" />
             <Button value="6" />
-        </div>
-        <div className="btn-wrapper">
+        </ButtonWrapper>
+        <ButtonWrapper>
             <Button value="7" />
             <Button value="8" />
             <Button value="9" />
-        </div>
-        <div className="btn-wrapper">
+        </ButtonWrapper>
+        <ButtonWrapper>
             <Button value="0" />
             <Button value="+" />
             <Button value="-" />
-        </div>
-        <div className="btn-wrapper">
+        </ButtonWrapper>
+        <ButtonWrapper>
             <Button value="/" />
             <Button value="*" />
             <Button value="=" />
-        </div>        
-    </div>;
+        </ButtonWrapper>
+      </div>  
+    );    
 }
 
 export default Calculator;
