f4e9829bc91457574add8619a9657e0448025bc0
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2023-10-11 10:45:33 -0600

    feat: other option adding the typing in the name of the component

--- taller_next/next-with-ts/react-with-ts/myproject/src/components/Button.tsx
+++ taller_next/next-with-ts/react-with-ts/myproject/src/components/Button.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
                    // this comes from props.value
-const Button: FC = ({ value }: { value: string }): JSX.Element => {
+const Button: FC<{ value: string }> = ({ value }): JSX.Element => {
     return <button className="calc-btn border">{ value }</button>
 }
