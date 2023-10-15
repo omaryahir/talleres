fcfc5a5ad951eb6a03014b74bc5a8905e0166bdc
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2023-10-15 15:10:50 -0600

    feat: adding login with scopes i reached the problem !!

---
+++ taller_gcp/curl-login-with-scopes.sh
@@ -0,0 +1,1 @@
+gcloud auth application-default login --scopes=https://www.googleapis.com/auth/admin.directory.rolemanagement.readonly
--- taller_gcp/curl-roles.sh
+++ taller_gcp/curl-roles.sh
@@ -1,3 +1,3 @@
 curl -H "X-Goog-User-Project: fakesi-testproject-dev" -H \
-"Authorization: Bearer $(gcloud auth print-access-token)" \
+"Authorization: Bearer $(gcloud auth print-access-token --scopes=[('https://www.googleapis.com/auth/admin.directory.rolemanagement.readonly')])" \
 https://admin.googleapis.com/admin/directory/v1/customer/my_customer/roles
