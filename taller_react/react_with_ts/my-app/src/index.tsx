a7046ed270da5cd2ca15f06f8aadda7a2f44d47b
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2023-10-21 19:23:14 -0600

    feat: running the first Test example

---
+++ taller_react/react_with_ts/my-app/src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client"
+
+const rootContainer = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(rootContainer)
+
+root.render("Hello World")
