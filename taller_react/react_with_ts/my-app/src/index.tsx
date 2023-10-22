c1f2530a97aa5afdc45c65b93504923a89d3571b
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2023-10-21 20:32:22 -0600

    feat: testing an App in react with TS

--- taller_react/react_with_ts/my-app/src/index.tsx
+++ taller_react/react_with_ts/my-app/src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client"
+import App from "./App";
 
 const rootContainer = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(rootContainer)
 
-root.render("Hello World")
+root.render(<App />)
